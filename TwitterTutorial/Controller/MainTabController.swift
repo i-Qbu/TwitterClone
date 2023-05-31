132a776bd732a7176c32ce96e36ba7cc6d749606
渡辺大智 <grandline.dai1@gmail.com>
2023-05-31 21:16:47 +0900

    Initial commit
    main画面をTabBarControllerにSceneDelegateを使用して指定.

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by 渡辺大智 on 2023/05/31.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
